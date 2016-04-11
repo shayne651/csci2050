a5c3691eac7a9f9a72a0ae22d825cb7e27ef1662
Shayne Taylor <shayne651@gmail.com>
2016-04-11 16:16:46 -0400

    encryption with key padding working ****one letter doesnt work always the last one

--- console32/console32/main.asm
+++ console32/console32/main.asm
@@ -17,34 +17,38 @@ test1:
 ;;;;;;;;This is sending text to the console and getting the key/message;;;;
 	invoke StdOut , ADDR getString
 	invoke StdIn , ADDR plainText,51
+	mov esi , eax
 	invoke StdOut , ADDR getKey
 	invoke StdIn , ADDR key,51
 ;;;;;;;;;done getting the plaintext/key;;;;;;;;
 
-	mov ebx ,SIZEOF plainText
+;;;;;;;;;;;padding key;;;;;;;;;;;;;;;;
+
+sub esi , eax
+	mov ecx , esi
+	mov esi,0
+	mov edi,eax
+	
+padKey:
+	mov al , [key+esi]
+	mov [key + edi] , al
+	inc esi
+	inc edi
+	loop padKey
+
+;;;;;;;;;;;/end padding key;;;;;;;;
+
+
+;;;;;;;;;encrypting the message;;;;;;;;;;;;;
+	mov esi , 10
 	mov edi , 0
 encrypt:
-	lea eax , [key + edi]
-	push eax
-    call letterSwitch
-	inc edi 
-	cmp ebx,0
-	je finish
-	dec ebx 
-	jmp encrypt
 
-finish:
-	ret
-main endp
-
-
-letterSwitch proc
-
-	mov ecx,0
+    mov ecx,0
 	mov dl , 0
-	mov cl , [key]
+	mov cl , [key + edi]
 plainAdd:
-	mov al , [plainText]
+	mov al , [plainText + edi]
 
 keyAdd:
 	mov bl ,[alph]
@@ -55,16 +59,29 @@ keyAdd:
 
 check:
 	add dl,al
-	cmp dl, 7Ah
+	cmp dl, "z"
 	jle finish
 	sub dl,26
 
 finish:
-
+	
 	mov jkd , dl
-	lea eax, jkd
-	invoke StdOut,ADDR jkd
+	invoke StdOut , ADDR [jkd]
 
-letterSwitch endp
 
+	inc edi 
+	cmp ebx,0
+	je Finish
+	dec esi 
+	cmp esi , 0
+	jnz encrypt
+
+;;;;;;;;;encryption finished;;;;;;;;;;;;;
+
+Finish:
+	invoke StdOut , ADDR jkd
+
+	mov eax,0
+	ret
+main endp
 end main
