4d917847188ed289a863c4d56545b89b82ff2623
Shayne Taylor <shayne651@gmail.com>
2016-04-11 13:43:57 -0400

    working for some values but on values such as e the register ecx and esi add improperly

--- console32/console32/main.asm
+++ console32/console32/main.asm
@@ -2,72 +2,68 @@ include \masm32\include\masm32rt.inc
 
 .data
 plainText BYTE 51 DUP(0),0
-getString BYTE "please enter a String: ",0
-key BYTE 51 DUP(0),0
-getKey BYTE "please enter a String ",0
-alph BYTE "abcdefghijklmnopqrstuvwxyz",0
-getMessageS BYTE "how many characters is your message (max 50)"
-addPlain DWORD 0
-addKey DWORD 0
-messageSize DWORD 0
-
+getString db "Please enter a String (1-50 chars): ",0
+cipherText BYTE 51 DUP(0),0
+key db 51 DUP(0),0
+getKey db "please enter a key (has to be the same size as the message): ",0
+alph BYTE "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",0
+jkd BYTE 51 DUP(0),0
+newLine BYTE 0ah,0dh,0
 
 .code 
-getPlain proc
-
-	mov al, [plainText]
-	mov ecx , 26
-
-plainLoop:
-	mov bl , [alph + ecx]
-	cmp bl,al
-	push ecx
-	pop addPlain
-	invoke StdOut , ADDR [ecx]
-	je keyLoop
-	loop restartLoop
-
-restartLoop:
-	mov ecx,26
-keyLoop:
-	mov bl , [alph + ecx]
-	cmp bl,al
-	je newLetter
-	loop keyLoop
-
-
-newLetter:
-	mov eax , addKey
-	add eax , addPlain
-	cmp eax , 26
-	jle Finish
-	jmp Greater
-
-Greater:
-	sub eax , 26
-	mov ecx , 26
-
-Finish:
-	invoke StdOut , ADDR [addKey]
-	pop eax
-	ret
-	
-getPlain endp
 main proc
 
-	invoke StdOut , ADDR getMessageS
-	invoke StdIn , ADDR messageSize,50
+test1:
+;;;;;;;;This is sending text to the console and getting the key/message;;;;
 	invoke StdOut , ADDR getString
-	invoke StdIn , ADDR plainText,50
+	invoke StdIn , ADDR plainText,51
 	invoke StdOut , ADDR getKey
-	invoke StdIn , ADDR key,50
+	invoke StdIn , ADDR key,51
+;;;;;;;;;done getting the plaintext/key;;;;;;;;
+	mov ebx , SIZEOF plainText
+	mov ecx , 1
+    call letterSwitch
 
-;encrypt:
-	call getPlain
-
-	;loop enctypt
-	
-	mov eax,0
 	ret
 main endp
+
+
+letterSwitch proc
+
+pop ecx
+mov ecx,25
+mov al , [plainText]
+mov edi , 0
+mov esi , 0
+PlainText:
+	mov bl , [alph+edi]
+	cmp bl,al
+	je KeyText
+	inc edi
+	loop PlainText
+
+resetLoop:
+	mov ecx, 25
+	mov al , [key]
+
+KeyText:
+	mov bl , [alph + esi]
+	cmp al,bl
+	je check
+	inc esi
+	loop KeyText
+
+check:
+	add esi , edi
+	cmp esi,25
+	jl Finish
+	sub esi , 25
+	
+Finish:
+	mov al , [alph + esi]
+	;invoke StdOut , ADDR [esi]
+	mov [jkd] , al
+	invoke StdOut , ADDR [jkd]
+letterSwitch endp
+
 end main
